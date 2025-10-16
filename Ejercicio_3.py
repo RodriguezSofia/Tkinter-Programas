2f55e93881558db122665c93795633a661f0c329
Sofia Rodriguez <anasofiarodriguez559@gmail.com>
2025-10-16 10:30:36 -0500

    Ejercicio actualizado

--- Ejercicio_3.py
+++ Ejercicio_3.py
@@ -2,7 +2,7 @@ import tkinter
 ventana=tkinter.Tk()
 ventana.geometry("600x400")
 ventana.title("Ejercicio 3")
-#TITULO
+#Titulo
 etiqueta=tkinter.Label(ventana, text="Lista de tareas", fg="red", font=("Arial", 16," bold"))
 etiqueta.pack()
 
@@ -12,31 +12,29 @@ etiqueta_tarea.pack()
 tarea=tkinter.Entry(ventana,font=("Arial", 12))
 tarea.pack()
 
-#LISTA DE TAREAS
-titulo_lista=tkinter.Label(ventana, text="Tareas:", font=("Arial", 12, "bold"))
-titulo_lista.pack()
+#Lista de tareas
+etiqueta_lista=tkinter.Label(ventana, text="Tareas:", font=("Arial", 12, "bold"))
+etiqueta_lista.pack()
 lista_tareas=tkinter.Listbox(ventana, font=("Arial", 12), width=40, height=10)
-lista_tareas.pack()
+lista_tareas.pack() #Listbox sirve para crear listas
 
-#AGREGAR TAREA
+#---------FUNCIONES---------
+#Agregar tarea
 def agregar_tarea():
     nueva_tarea=tarea.get()
-    if nueva_tarea.strip() != "":
-        lista_tareas.insert(tkinter.END, nueva_tarea)
-        tarea.delete(0, tkinter.END)
-
-#ELIMINAR TAREA
+    lista_tareas.insert(tkinter.END, nueva_tarea) #insertar al final en la lista
+    tarea.delete(0, tkinter.END) #para limpiar la entrada despues de agregar
+#Eliminar tarea
 def eliminar_tarea():
-    tarea_seleccionada=lista_tareas.curselection()
-    if tarea_seleccionada:
-        lista_tareas.delete(tarea_seleccionada)
+    tarea_seleccionada=lista_tareas.curselection() #para obtener la tarea seleccionada con el cursor
+    lista_tareas.delete(tarea_seleccionada) #elimina la tarea seleccionada
 
-#BOTON "ELIMINAR"
-boton_eliminar=tkinter.Button(ventana, text="Eliminar", command= eliminar_tarea, bg="lightcoral", padx=10, pady=5)
-boton_eliminar.pack()
-
-#BOTON "AGREGAR"
-boton_agregar=tkinter.Button(ventana, text="Agregar", command= agregar_tarea, bg="lightgreen", padx=30,pady=5)
+#---------BOTONES---------
+#Boton "AGREGAR"
+boton_agregar=tkinter.Button(ventana, text="Agregar", fg="green", font= ("Arial", 12, "bold" ), command= agregar_tarea, bg="lightgreen", padx=30,pady=5)
 boton_agregar.pack()
+#Boton "ELIMINAR"
+boton_eliminar=tkinter.Button(ventana, text="Eliminar", fg="red", font= ("Arial", 12, "bold" ), command= eliminar_tarea, bg="lightcoral", padx=30, pady=5)
+boton_eliminar.pack()
 
 ventana.mainloop()
